f8c5ba76628e3f657abbe34f377cccb63e3d1786
fjggy <fjggy@aya.yale.edu>
2025-04-05 10:58:19 -0700

    hello world

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -4,11 +4,11 @@ app.http('message', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-       // context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url "${request.url}"`);
 
-       // const name = request.query.get('name') || await request.text() || 'world';
+        const name = request.query.get('name') || await request.text() || 'world';
 
-       // return { body: `Hello, ${name}!` };
-        return { body: `{ "text": "Hello, from the API!" }` };
+        return { body: `Hello, ${name}!` };
+       // return { body: `{ "text": "Hello, from the API!" }` };
     }
 });
