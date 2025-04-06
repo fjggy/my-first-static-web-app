1760a64fe7bb7e9e6f103ddf485585c5c714243d
fjggy <fjggy@aya.yale.edu>
2025-04-06 15:17:26 -0700

    azure function created in VS code

---
+++ src/functions/submitUser2.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? `Hello, ${name}. This HTTP triggered function executed successfully.`
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        status: 200,  // Explicit status code
+        headers: {
+            'Content-Type': 'application/json'  // Ensure the response is JSON
+        },
+        body: JSON.stringify({ message: responseMessage })  // Return as JSON
+    };
+};
